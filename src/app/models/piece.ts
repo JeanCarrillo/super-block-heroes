4fb6f1c57dff1d9d13581d1274c1caf2958eba33
JeanCarrillo <jeancarrillo@gmail.com>
2019-10-07 15:10:57 +0200

    first rotation version, collisions not handled yet

--- src/app/models/piece.ts
+++ src/app/models/piece.ts
@@ -1,112 +1,54 @@
-const pieces = [
-  {
-    type: 'T',
-    shapes: [
-      // default
-      [1, 1, 1], [0, 1, 0], [0, 0, 0],
-      // rotation 1 right
-      [0, 0, 1], [0, 1, 1], [0, 0, 1],
-      // rotation 2 right
-      [0, 0, 0], [0, 1, 0], [1, 1, 1],
-      //roration 3 right
-      [1, 0, 0], [1, 1, 0], [1, 0, 0]
-    ]
-  },
-
-  {
-    type: 'Z',
-    shapes: [
-      //default
-      [1, 1, 0], [0, 1, 1], [0, 0, 0],
-      //rotation 1 right
-      [0, 0, 1], [0, 1, 1], [0, 1, 0],
-      //rotaion 2 right
-      [0, 0, 0], [1, 1, 0], [0, 1, 1],
-      //roration 3 right
-      [0, 1, 0], [1, 1, 0], [1, 0, 0]
-    ]
-  },
-
-  {
-    type: 'S',
-    shapes: [
-      //default
-      [0, 1, 1], [1, 1, 0], [0, 0, 0],
-      //rotation 1 right
-      [0, 1, 0], [0, 1, 1], [0, 0, 1],
-      //rotaion 2 right
-      [0, 0, 0], [0, 1, 1], [1, 1, 0],
-      //roration 3 right
-      [1, 0, 0], [1, 1, 0], [0, 1, 0]
-    ]
-  },
-
-  {
-    type: 'L',
-    shapes: [
-      //default
-      [1, 0, 0], [1, 0, 0], [1, 1, 0],
-      //rotation 1 right
-      [1, 1, 1], [1, 0, 0], [0, 0, 0],
-      //rotaion 2 right
-      [0, 1, 1], [0, 0, 1], [0, 0, 1],
-      //roration 3 right
-      [0, 0, 0], [0, 0, 1], [1, 1, 1]
-    ]
-  },
-
-  {
-    type: 'J',
-    shapes: [
-      //default
-      [0, 0, 1], [0, 0, 1], [0, 1, 1],
-      //rotation 1 right
-      [0, 0, 0], [1, 0, 0], [1, 1, 1],
-      //rotaion 2 right
-      [1, 1, 0], [1, 0, 0], [1, 0, 0],
-      //roration 3 right
-      [1, 1, 1], [0, 0, 1], [0, 0, 0]
-    ]
-  },
-
-  {
-    type: 'O',
-    shapes: [
-      //default and unique shape
-      [1, 1], [1, 1]
-    ]
-  },
-
-  {
-    type: 'I',
-    shapes:[
-      //default
-      [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0],
-      //rotation 1 right
-      [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0],
-      //rotaion 2 right
-      [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0],
-      //roration 3 right
-      [0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]
-    ]
-  }
-];
+import { pieces } from './pieces';
 
 export class Piece {
-  type: string;
+  x: number;
+  y: number;
   shape: number[][];
+  rotationIndex: number;
+  pieceIndex: number;
+  color: any;
 
-  constructor() {
-    const color = Math.ceil(Math.random() * 4);
-    const rdm = Math.floor(Math.random() * pieces.length);
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+    this.rotationIndex = 0;
+    this.color = Math.ceil(Math.random() * 4);
+    // random piece index, T = 1, Z = 2 etc.
+    this.pieceIndex = Math.floor(Math.random() * pieces.length);
+    this.createShape();
+    // type (string), (example: "J")
+  }
+
+  public rotate(num: number) {
+    let index = this.rotationIndex + num;
+    if (!pieces[this.pieceIndex].shapes[this.rotationIndex + num]) {
+      index = 0;
+    }
+    this.rotationIndex = index;
+    this.createShape();
+  }
+
+  public createShape() {
     const shape = [];
-    for (let i = 0; i < pieces[rdm].shape.length; i++) {
+    for (
+      let i = 0;
+      i < pieces[this.pieceIndex].shapes[this.rotationIndex].length;
+      i++
+    ) {
       shape.push([]);
-      for (let j = 0; j < pieces[rdm].shape[i].length; j++) {
-        shape[i].push(pieces[rdm].shape[i][j] === 0 ? 0 : color);
+      // tslint:disable-next-line: prefer-for-of
+      for (
+        let j = 0;
+        j < pieces[this.pieceIndex].shapes[this.rotationIndex][i].length;
+        j++
+      ) {
+        shape[i].push(
+          pieces[this.pieceIndex].shapes[this.rotationIndex][i][j] === 0
+            ? 0
+            : this.color
+        );
       }
     }
     this.shape = shape;
-    this.type = pieces[rdm].type;
   }
 }
--- src/app/pages/game/board/board.component.ts
+++ src/app/pages/game/board/board.component.ts
@@ -6,7 +6,10 @@ import { Piece } from '../../../models/piece';
 // Keyboard buttons
 export enum KEY_CODE {
   RIGHT_ARROW = 39,
-  LEFT_ARROW = 37
+  LEFT_ARROW = 37,
+  UP_ARROW = 38,
+  DOWN_ARROW = 40,
+  SPACEBAR = 32
 }
 
 @Component({
@@ -15,8 +18,11 @@ export enum KEY_CODE {
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit, OnDestroy {
+  score: number;
   board: Board;
   currentBlocks: Block[];
+  currentPiece: Piece;
+  // currentShape: any;
   interval: any;
   fallingSpeed: number;
 
@@ -31,10 +37,15 @@ export class BoardComponent implements OnInit, OnDestroy {
     if (event.keyCode === KEY_CODE.LEFT_ARROW) {
       this.moveCurrentBlocks(-1);
     }
+    // tslint:disable-next-line: deprecation
+    if (event.keyCode === KEY_CODE.SPACEBAR) {
+      this.rotateCurrentPiece(1);
+    }
   }
 
   constructor() {
     this.fallingSpeed = 200;
+    this.score = 0;
     const rowNumbers = 10;
     const colNumbers = 20;
     this.board = new Board(rowNumbers, colNumbers);
@@ -68,12 +79,29 @@ export class BoardComponent implements OnInit, OnDestroy {
     if (this.currentBlocks.length > 0) {
       return;
     }
-    const newPiece = new Piece();
-    for (let i = 0; i < newPiece.shape.length; i++) {
-      for (let j = 0; j < newPiece.shape[i].length; j++) {
-        const color = newPiece.shape[i][j];
+    this.currentPiece = new Piece(5, 0);
+    for (let i = 0; i < this.currentPiece.shape.length; i++) {
+      for (let j = 0; j < this.currentPiece.shape[i].length; j++) {
+        const color = this.currentPiece.shape[i][j];
         if (color !== 0) {
           this.currentBlocks.push(new Block(color, 5 + i, 0 + j));
+        }
+      }
+    }
+  }
+
+  // direction: number of indexes to rotate
+  // => direction 1 : rotate right, direction -1 : rotate left
+  private rotateCurrentPiece(direction: number) {
+    this.currentPiece.rotate(direction);
+    this.currentBlocks = [];
+    for (let i = 0; i < this.currentPiece.shape.length; i++) {
+      for (let j = 0; j < this.currentPiece.shape[i].length; j++) {
+        const color = this.currentPiece.shape[i][j];
+        if (color !== 0) {
+          this.currentBlocks.push(
+            new Block(color, this.currentPiece.x + i, this.currentPiece.y + j)
+          );
         }
       }
     }
@@ -83,14 +111,41 @@ export class BoardComponent implements OnInit, OnDestroy {
     for (let i = 0; i < this.currentBlocks.length; i++) {
       this.currentBlocks[i].y += 1;
     }
+    console.log(this.currentPiece);
+    if (this.currentPiece) {
+      this.currentPiece.y += 1;
+    }
   }
 
   private handleCollision(): void {
     for (let i = 0; i < this.currentBlocks.length; i++) {
       const block = this.currentBlocks[i];
+      // Current block is now part of the board matrix
       this.board.tiles[block.y][block.x] = block.color;
     }
     this.currentBlocks = [];
+    this.checkCompletedLines();
+  }
+
+  private checkCompletedLines() {
+    for (let i = 0; i < this.board.tiles.length; i++) {
+      let completed = true;
+      for (let j = 0; j < this.board.tiles[i].length; j++) {
+        if (this.board.tiles[i][j] === 0) {
+          completed = false;
+          break;
+        }
+      }
+      if (completed) {
+        this.deleteRow(i);
+      }
+    }
+  }
+
+  private deleteRow(index: number) {
+    for (let i = 0; i < this.board.tiles[index].length; i++) {
+      this.board.tiles[index][i] === 0;
+    }
   }
 
   // Returns true if something is colliding
@@ -139,6 +194,9 @@ export class BoardComponent implements OnInit, OnDestroy {
     if (canMove) {
       for (const block of this.currentBlocks) {
         block.x += x;
+      }
+      if (this.currentPiece) {
+        this.currentPiece.x += x;
       }
     }
   }
