1c12cf6d979e93b5fb9c9656f412fc2e00195c47
JeanCarrillo <jeancarrillo@gmail.com>
2020-01-06 16:52:53 +0100

    socket test in sign-in

--- src/app/pages/authentification/sign-in/sign-in.component.ts
+++ src/app/pages/authentification/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../shared/services/auth.service';
-import { Router } from '@angular/router';
+import { SocketService } from '../../../shared/services/socket.service';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 // import { Monster } from 'src/app/shared/models/monster';
 import { ShowPasswordDirective } from '../show-password.directive';
@@ -18,9 +18,12 @@ export class SignInComponent implements OnInit {
   faEye = faEye;
   showPasswordDirective = ShowPasswordDirective;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  constructor(private authService: AuthService, private socketService: SocketService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.socketService.sendMessage('hello');
+    this.socketService.getMessage().subscribe(response => console.log({ response }));
+  }
 
   forgotPW = () => {
     window.alert('This feature will be enable soon!');
