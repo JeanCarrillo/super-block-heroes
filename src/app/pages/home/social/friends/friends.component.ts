f014b2cd046d5cb509de8faf34adbf9332a213e8
Léopold Assogba <leopoldassogba21@gmail.com>
2020-01-10 15:47:08 +0100

    change social view

---
+++ src/app/pages/home/social/friends/friends.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../../../shared/services/auth.service';
+
+@Component({
+  selector: 'app-friends',
+  templateUrl: './friends.component.html',
+  styleUrls: ['./friends.component.css']
+})
+export class FriendsComponent implements OnInit {
+
+  constructor(private authservice: AuthService) { }
+
+  ngOnInit() {
+  }
+
+}
