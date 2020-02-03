d6b11034380a7f93a769c43d6d8dedd1c541f1d1
Léopold Assogba <leopoldassogba21@gmail.com>
2020-02-03 13:01:19 +0100

    css capacity

--- src/app/shared/constants/keyboard.ts
+++ src/app/shared/constants/keyboard.ts
@@ -6,31 +6,31 @@ export const KEYS = [
     MOVE_LEFT: 'ArrowLeft',
     MOVE_RIGHT: 'ArrowRight',
     INSTANT_DROP: 'ArrowUp',
-    CAPACITY: 'n',
+    CAPACITY: 'Shift',
     ROTATE_RIGHT: ' ',
   },
-  // Player 2
-  {
-    MOVE_LEFT: 'q',
-    MOVE_RIGHT: 'd',
-    INSTANT_DROP: 'z',
-    ROTATE_RIGHT: 'Shift',
-    CAPACITY: 'a',
-  },
-  // Player 3
-  {
-    MOVE_LEFT: '',
-    MOVE_RIGHT: '',
-    INSTANT_DROP: '',
-    ROTATE_RIGHT: '',
-    CAPACITY: '',
-  },
-  // Player 4
-  {
-    MOVE_LEFT: '',
-    MOVE_RIGHT: '',
-    INSTANT_DROP: '',
-    ROTATE_RIGHT: '',
-    CAPACITY: '',
-  },
+  // // Player 2
+  // {
+  //   MOVE_LEFT: 'q',
+  //   MOVE_RIGHT: 'd',
+  //   INSTANT_DROP: 'z',
+  //   ROTATE_RIGHT: 'Shift',
+  //   CAPACITY: 'a',
+  // },
+  // // Player 3
+  // {
+  //   MOVE_LEFT: '',
+  //   MOVE_RIGHT: '',
+  //   INSTANT_DROP: '',
+  //   ROTATE_RIGHT: '',
+  //   CAPACITY: '',
+  // },
+  // // Player 4
+  // {
+  //   MOVE_LEFT: '',
+  //   MOVE_RIGHT: '',
+  //   INSTANT_DROP: '',
+  //   ROTATE_RIGHT: '',
+  //   CAPACITY: '',
+  // },
 ];
